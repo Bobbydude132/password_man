03aa2ca770f36c79cd0f457e289d1af7d4107b7a
Bobbydude132 <flashton123@yahoo.com>
2025-04-19 10:20:01 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual';  // Prevent automatic scroll restoration
 }
-
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.anim').forEach(el => {
         el.classList.add('load');
   });
 });
-
 function showalert() {
     alert("Linux is under development. Try again later.");
 }
+
